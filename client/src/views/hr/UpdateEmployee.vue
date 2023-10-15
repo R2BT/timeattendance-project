40e5cae3f48746ce5b73dd99be3e13091be789bb
autsadawut.kl <autsadawut.dew10@gmail.com>
2023-10-15 21:12:31 +0700

    update UpdateEmployee

--- client/src/views/hr/UpdateEmployee.vue
+++ client/src/views/hr/UpdateEmployee.vue
@@ -34,7 +34,7 @@
                   ]" />
 
                   <q-input color="purple-8" v-model="employeeData.birthdate" label="วันเกิด" type="date" lazy-rules="..."
-                    mask="####-##-##" />
+                    mask="####-##-##" @input="updateBirthdate" />
                   <q-input color="purple-8" v-model="employeeData.salary" label="เงินเดือน" lazy-rules :rules="[
                     val => /^\d+(\.\d{1,2})?$/.test(val) || 'รูปแบบเงินเดือนไม่ถูกต้อง (ตัวอย่าง: 5000 หรือ 5000.50)'
                   ]" />
@@ -66,7 +66,7 @@
                     val => /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(val) || 'รูปแบบอีเมลไม่ถูกต้อง'
                   ]" />
                   <q-input color="purple-8" v-model="employeeData.startDate" label="วันเริ่มงาน" type="date"
-                    lazy-rules="..." mask="####-##-##" />
+                    lazy-rules="..." mask="####-##-##" @input="updateStartDate" />
                   <q-input color="purple-8" v-model="employeeData.idCard" label="รหัสบัตรประชาชน" lazy-rules :rules="[
                     val => val && val.length === 13 && /^[0-9]+$/.test(val) || 'รหัสบัตรประชาชนไม่ถูกต้อง'
                   ]" />
@@ -82,8 +82,8 @@
           flex-grow: 1; height: fit-content;">
               <q-card-section>
                 <label class="form-label">ข้อมูลในบริษัท</label>
-                <q-input color="purple-8" v-model="employeeData.employeeId" label="รหัสพนักงาน" readonly/>
-                <q-input color="purple-8" v-model="employeeData.id" label="ID" readonly/>
+                <q-input color="purple-8" v-model="employeeData.employeeId" label="รหัสพนักงาน" readonly />
+                <q-input color="purple-8" v-model="employeeData.id" label="ID" readonly />
                 <q-input color="purple-8" v-model="employeeData.password" label="Password" lazy-rules
                   :rules="[val => val && val.length > 0 || 'กรุณากรอก Password']" />
                 <div style="margin: 10px;"></div>
@@ -96,6 +96,7 @@
                       </template>
                     </q-file>
                   </div>
+                  <div class="mb-2">{{ employeeData.employee_profile_img ? employeeData.employee_profile_img.replace(/^uploads\\/, '') : '' }}</div>
                 </div>
                 <div style="margin: 10px;"></div>
                 <div class="mb-3">
@@ -107,6 +108,7 @@
                       </template>
                     </q-file>
                   </div>
+                  <div class="mb-2">{{ employeeData.employee_personnel_id_img ? employeeData.employee_personnel_id_img.replace(/^uploads\\/, '') : '' }}</div>
                 </div>
                 <div style="margin: 10px;"></div>
                 <div class="mb-3">
@@ -118,6 +120,7 @@
                       </template>
                     </q-file>
                   </div>
+                  <div class="mb-2">{{ employeeData.employee_transcript_img ? employeeData.employee_transcript_img.replace(/^uploads\\/, '') : '' }}</div>
                 </div>
                 <div style="margin: 10px;"></div>
                 <div class="mb-3">
@@ -129,6 +132,7 @@
                       </template>
                     </q-file>
                   </div>
+                  <div class="mb-2">{{ employeeData.employee_contract ? employeeData.employee_contract.replace(/^uploads\\/, '') : '' }}</div>
                 </div>
                 <div style="margin: 15px;"></div>
                 <div class="q-gutter-md" style="display: flex; justify-content: end;">
@@ -171,7 +175,7 @@ export default {
       name: "",
       department: "",
       phoneNumber: "",
-      birthdate: "",
+      birthdate: null,
       salary: "",
       address: "",
       idCardBank: "",
@@ -179,7 +183,7 @@ export default {
       lastName: "",
       position: "",
       email: "",
-      startDate: "",
+      startDate: null,
       idCard: "",
       taxId: "",
       employeeId: "",
@@ -203,7 +207,8 @@ export default {
           employeeData.value.name = data.employee_name;
           employeeData.value.department = data.employee_department;
           employeeData.value.phoneNumber = data.employee_tel;
-          employeeData.value.birthdate = data.employee_birthday;
+          employeeData.value.startDate = new Date(data.employee_start_date);
+          employeeData.value.birthdate = new Date(data.employee_birthday);
           employeeData.value.salary = data.employee_salary;
           employeeData.value.address = data.employee_address;
           employeeData.value.idCardBank = data.employee_bank_account;
@@ -211,7 +216,6 @@ export default {
           employeeData.value.lastName = data.employee_surname;
           employeeData.value.position = data.employee_position;
           employeeData.value.email = data.employee_email;
-          employeeData.value.startDate = data.employee_start_date;
           employeeData.value.idCard = data.employee_personal_id;
           employeeData.value.taxId = data.employee_tax_id;
           employeeData.value.employeeId = data.employee_id;
@@ -233,42 +237,52 @@ export default {
       router.go(-1);
     };
 
-    const formData = new FormData();
+    const updateStartDate = (newDate) => {
+      // Format the new date as yyyy-mm-dd
+      employeeData.value.startDate = newDate.toISOString().split('T')[0];
+    };
 
-    formData.append('employee_gender', employeeData.selectedGender);
-    formData.append('employee_title', employeeData.preName);
-    formData.append('employee_name', employeeData.name);
-    formData.append('employee_surname', employeeData.lastName);
-    formData.append('employee_personal_id', employeeData.idCard);
-    formData.append('employee_tel', employeeData.phoneNumber);
-    formData.append('employee_email', employeeData.email);
-    formData.append('employee_birthday', employeeData.birthdate);
-    formData.append('employee_address', employeeData.address);
-    formData.append('employee_position', employeeData.position);
-    formData.append('employee_department', employeeData.department);
-    formData.append('employee_start_date', employeeData.startDate);
-    formData.append('employee_salary', employeeData.salary);
-    formData.append('employee_bank_account', employeeData.idCardBank);
-    formData.append('employee_tax_id', employeeData.taxId);
-    formData.append('employee_bank_type', 'SCB');
-    formData.append('employee_id', employeeData.employeeId);
-    formData.append('employee_login_id', employeeData.id);
-    formData.append('employee_login_password', employeeData.password);
-
-    // เพิ่มการส่งไฟล์
-    formData.append('employee_profile_img', employeeData.employee_profile_img);
-    formData.append('employee_personnel_id_img', employeeData.employee_personnel_id_img);
-    formData.append('employee_transcript_img', employeeData.employee_transcript_img);
-    formData.append('employee_contract', employeeData.employee_contract);
+    const updateBirthdate = (newDate) => {
+      // Format the new date as yyyy-mm-dd
+      employeeData.value.birthdate = newDate.toISOString().split('T')[0];
+    };
 
     const saveEmployeeData = async () => {
       try {
+        const formData = new FormData();
+
+        formData.append('employee_gender', employeeData.value.selectedGender);
+        formData.append('employee_title', employeeData.value.preName);
+        formData.append('employee_name', employeeData.value.name);
+        formData.append('employee_surname', employeeData.value.lastName);
+        formData.append('employee_personal_id', employeeData.value.idCard);
+        formData.append('employee_tel', employeeData.value.phoneNumber);
+        formData.append('employee_email', employeeData.value.email);
+        formData.append('employee_birthday', employeeData.value.birthdate);
+        formData.append('employee_address', employeeData.value.address);
+        formData.append('employee_position', employeeData.value.position);
+        formData.append('employee_department', employeeData.value.department);
+        formData.append('employee_start_date', employeeData.value.startDate);
+        formData.append('employee_salary', employeeData.value.salary);
+        formData.append('employee_bank_account', employeeData.value.idCardBank);
+        formData.append('employee_tax_id', employeeData.value.taxId);
+        formData.append('employee_bank_type', 'SCB');
+        formData.append('employee_id', employeeData.value.employeeId);
+        formData.append('employee_login_id', employeeData.value.id);
+        formData.append('employee_login_password', employeeData.value.password);
+
+        // เพิ่มการส่งไฟล์
+        formData.append('employee_profile_img', employeeData.value.employee_profile_img);
+        formData.append('employee_personnel_id_img', employeeData.value.employee_personnel_id_img);
+        formData.append('employee_transcript_img', employeeData.value.employee_transcript_img);
+        formData.append('employee_contract', employeeData.value.employee_contract);
         const response = await axios.put(
           "http://localhost:3000/employee/" + router.currentRoute.value.params.id,
           formData
         );
 
         if (response.status === 200) {
+          goBack();
           console.log("Data updated successfully");
         } else {
           console.error("Failed to update data");
@@ -282,7 +296,28 @@ export default {
       employeeData,
       goBack,
       saveEmployeeData,
+      updateStartDate,
+      updateBirthdate,
     };
+  },
+  handleProfileImageUpload(event) {
+    // Handle the file selection for the profile image
+    this.employee_profile_img = event.target.files[0];
+  },
+
+  handlePersonnelIdImageUpload(event) {
+    // Handle the file selection for the personnel ID image
+    this.employee_personnel_id_img = event.target.files[0];
+  },
+
+  handleTranscriptImageUpload(event) {
+    // Handle the file selection for the transcript image
+    this.employee_transcript_img = event.target.files[0];
+  },
+
+  handleContractUpload(event) {
+    // Handle the file selection for the contract
+    this.employee_contract = event.target.files[0];
   },
 };
 </script>
